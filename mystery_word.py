158b83858f56f6c95207081946c01facdd79c94e
Alexis Mainwaring <leximainw@gmail.com>
2022-08-15 12:02:50 -0400

    allow user to guess letters

--- mystery_word.py
+++ mystery_word.py
@@ -2,6 +2,9 @@ import random
 import re
 
 
+MAX_ERRORS = 8
+
+
 def play_game():
     all_phrases = import_phrases()
     while True:
@@ -10,10 +13,24 @@ def play_game():
         errors = 0
         won = False
         display_board(curr_phrases[0], guessed_letters)
-        while errors < 8 and not won:
-            pass
-            # guess = prompt_guess()
-            # display_board(curr_phrases[0], guessed_letters)
+        while errors < MAX_ERRORS and not won:
+            guess = prompt_guess()
+            if guess not in guessed_letters:
+                curr_phrases, correct = check_guess(curr_phrases, guess)
+                guessed_letters.append(guess)
+                if not correct:
+                    errors += 1
+                    if errors >= MAX_ERRORS:
+                        print("Game over!")
+                        print(f"The word was: {curr_phrases[0]}")
+                        break
+            if not display_board(curr_phrases[0], guessed_letters):
+                won = True
+                print("You win!")
+
+
+def check_guess(curr_phrases, guess):
+    return (curr_phrases, guess in list(curr_phrases[0]))
 
 
 def display_board(phrase, guessed):
@@ -21,12 +38,22 @@ def display_board(phrase, guessed):
     wrong_guesses = [letter for letter in guessed if letter not in list(phrase)]
     chars = list(phrase)
     display = []
+    missing = False
     for char in chars:
         if char in guessed or re.match('[^A-Za-z]', char):
             display.append(char)
         else:
+            missing = True
             display.append('_')
-    print(''.join(display))
+    print(f"{''.join(display)}   Wrong: {''.join(wrong_guesses)}")
+    return missing
+
+
+def prompt_guess():
+    guess = ''
+    while not guess or len(guess) != 1:
+        guess = input("Guess: ")
+    return guess
 
 
 def import_phrases():
