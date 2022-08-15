e8b842d1bf9e2b80a383375fa33e91e95d1b50ce
Alexis Mainwaring <leximainw@gmail.com>
2022-08-15 11:48:48 -0400

    display the current phrase

--- mystery_word.py
+++ mystery_word.py
@@ -1,11 +1,32 @@
 import random
+import re
 
 
 def play_game():
     all_phrases = import_phrases()
     while True:
         curr_phrases = [random.choice(all_phrases)]
-        print(curr_phrases[0])
+        guessed_letters = []
+        errors = 0
+        won = False
+        display_board(curr_phrases[0], guessed_letters)
+        while errors < 8 and not won:
+            pass
+            # guess = prompt_guess()
+            # display_board(curr_phrases[0], guessed_letters)
+
+
+def display_board(phrase, guessed):
+    phrase = phrase.replace('_', ' ')
+    wrong_guesses = [letter for letter in guessed if letter not in list(phrase)]
+    chars = list(phrase)
+    display = []
+    for char in chars:
+        if char in guessed or re.match('[^A-Za-z]', char):
+            display.append(char)
+        else:
+            display.append('_')
+    print(''.join(display))
 
 
 def import_phrases():
