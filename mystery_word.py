3f96886aa3db4a78acb2876633e4019ff87094d1
Alexis Mainwaring <leximainw@gmail.com>
2022-08-15 11:39:50 -0400

    import words and choose a random word each round

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,19 @@
+import random
+
+
 def play_game():
-    pass
+    all_phrases = import_phrases()
+    while True:
+        curr_phrases = [random.choice(all_phrases)]
+        print(curr_phrases[0])
+
+
+def import_phrases():
+    phrases = []
+    handle = open("words.txt", "r")
+    while line := handle.readline():
+        phrases.append(line.strip())
+    return phrases
 
 
 if __name__ == "__main__":
